f782123e65e807dc4917cc9948e6225b0f44a25a
UBF21 <flashgames104@gmail.com>
2025-05-19 19:34:37 -0500

    Correcciones de colores y enfasis de texto y lógica de navegación de retorno (caso particular)

--- src/core/features/plans/presentation/Layout-PlanView.tsx
+++ src/core/features/plans/presentation/Layout-PlanView.tsx
@@ -29,23 +29,23 @@ export const LayoutPlanView = () => {
             clearPlan;
             clearPlanOption;
             useStepStore.setState({ step: 0 })
-        }else{
+        } else {
             useStepStore.setState({ step: step - 1 });
         }
     }
 
     return (
         <div className="bg-light">
-            <div className="container-fluid d-flex justify-content-center align-items-center bg-primary-light">
+            <div className="container-fluid d-flex justify-content-center align-items-center bg-primary-light p-0">
                 <div className="w-75 mt-4 d-block  d-md-none">
                     <Stepper nonLinear activeStep={step} alternativeLabel>
                         <Step key={crypto.randomUUID()}>
                             <StepButton onClick={changePrevius} color="inherit" icon={<ArrowCircleLeftOutlinedIcon fontSize="large" className="text-primary-dark" />}></StepButton>
                         </Step>
                         {steps.map((label) => (
-                            <Step key={label} >
+                            <Step key={label}>
                                 <StepButton color="inherit">
-                                    {label}
+                                    <span className="font-weight-semibold text-dark">{label}</span>
                                 </StepButton>
                             </Step>
                         ))}
@@ -56,7 +56,7 @@ export const LayoutPlanView = () => {
                         {steps.map((label) => (
                             <Step key={label} >
                                 <StepButton color="inherit">
-                                    {label}
+                                    <span className="font-weight-semibold text-dark">{label}</span>
                                 </StepButton>
                             </Step>
                         ))}
@@ -67,14 +67,14 @@ export const LayoutPlanView = () => {
             <main>
                 <div className="container d-none d-md-block mt-4">
                     <a className="text-decoration-none cursor-pointer">
-                    <div className="d-flex align-items-center justify-content-start" onClick={changePrevius}>
-                        <div>
-                            <ArrowCircleLeftOutlinedIcon fontSize="large" className="text-blue-berry"/>
+                        <div className="d-flex align-items-center justify-content-start" onClick={changePrevius}>
+                            <div>
+                                <ArrowCircleLeftOutlinedIcon fontSize="large" className="text-blue-berry" />
+                            </div>
+                            <div>
+                                <span className="font-weight-bold text-xl text-blue-berry">Volver</span>
+                            </div>
                         </div>
-                        <div>
-                            <span className="font-weight-bold text-xl text-blue-berry">Volver</span>
-                        </div>
-                    </div>
                     </a>
                 </div>
 
